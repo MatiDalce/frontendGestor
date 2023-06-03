51ac1ba0ab4b1e397abdfeecef4097ea81692498
Matias <matigodoy93@hotmail.com>
2023-06-03 15:15:37 -0300

    webAPI desarrollo (produccion activa)

--- frontend/src/env/config.js
+++ frontend/src/env/config.js
@@ -1,6 +1,8 @@
 // URL de vinculación con Backend en desarrollo
-const webAPI = 'http://localhost:3006';
-// const webAPI = 'https://t.mdc.guest.o-o.fyi';
+// const webAPI = 'http://localhost:3006';
+
+// URL de vinculación con Backend en producción
+const webAPI = 'https://t.mdc.guest.o-o.fyi';
 
 export const config = {
     webAPI,
