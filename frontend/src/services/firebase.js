f87c030e3d7a8d28795b9f8bb0c613ac842cfa30
devel@mauriciocap.com <devel@mauriciocap.com>
2023-06-04 13:00:23 -0400

    OK: generar zip firebase FALTA traer los datos

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -3,7 +3,7 @@
 //VER: https://www.freecodecamp.org/news/use-firebase-authentication-in-a-react-app/
 import {  signInWithEmailAndPassword, signOut   } from 'firebase/auth';
 import { auth } from '../firebase.js'; //TODO: reunir todo aca
-
+import { zipSync, strToU8 } from 'fflate';
 
 export const backendSignin = (email, pass) => {
 	return signInWithEmailAndPassword(auth, email, pass)
@@ -25,11 +25,24 @@ export const backendSignout = () => {
 }
 	
 export const backendDownloadAppointments = () => {
-	return new Promise( (ok, err) => err('TODO: implementar') );
+	//VER: https://github.com/101arrowz/fflate
+	//TODO: traer los datos de verdad!	
+	const archivo= zipSync({ //A: el contenido del zip es un diccionario carpeta -> archivo -> contenido
+	'Paciente Pepe': [{
+    'Dia uno.txt': [strToU8('pepe hello world 1')],
+    'Dia dos.txt': [strToU8('pepe hello world 2')],
+   }],
+	'Paciente Juana': [{
+    'Dia uno.txt': [strToU8('juana hello world 1')],
+    'Dia dos.txt': [strToU8('juana hello world 2')],
+   }],
+  });
+	window.zip= archivo
+	return new Promise( (ok, err) => ok(zip) );
 }
 
 export const backendPatientAdd = async (data) => {
 	firebaseSet('patients','AUTO',data);
-	return {}
+	return {} //A: caller espera diccionario y 'errors' si hubo
 }
 
